44398b7626541cdce15ce3e17668c8a2378f055f
omer257 <omer257@gmail.com>
2017-07-11 14:25:07 +0300

    server ready

--- beerModel.js
+++ beerModel.js
@@ -1,1 +1,12 @@
+var beerSchema = new mongoose.Schema({
+  name: { type: String },
+  style: { type: String },
+  image_url: { type: String },
+  abv: { type: Number },
+  ratingTotal: { type: Number },
+  numberOfRatings: { type: Number }
+});
 
+var Beer = mongoose.model("Beer", beerSchema);
+
+module.exports = Beer;
